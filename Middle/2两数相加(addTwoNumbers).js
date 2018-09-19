6d1d37f646cae372ad37d8aa631639ec70fcd4ad
LinStan <596636876@qq.com>
2018-09-19 08:56:37 +0800

    rename

--- Middle/2两数相加(addTwoNumbers).js
+++ Middle/2两数相加(addTwoNumbers).js
@@ -2,6 +2,7 @@
   输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
   输出：7 -> 0 -> 8
   原因：342 + 465 = 807
+  链表还要好好学一下！
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
@@ -29,10 +30,10 @@ var addTwoNumbers = function(l1, l2) {
 
   if(l1 === null){return l2}
   else if(l2 === null){return l1}
-  while(cur1 || cur2)
+  while(cur1 || cur2)//若链表不为空，开始取值
   {
     var temp1,temp2;
-    if(cur1)
+    if(cur1)//l1不为空，获取l1的当前节点值
     {
       temp1 = cur1.val;
       cur1 = cur1.next;
@@ -40,7 +41,7 @@ var addTwoNumbers = function(l1, l2) {
     {
       temp1=0;
     }
-    if(cur2)
+    if(cur2)//l2不为空，获取l2的当前节点值
     {
       temp2 = cur2.val;
       cur2 = cur2.next;
@@ -51,7 +52,7 @@ var addTwoNumbers = function(l1, l2) {
     }
     var sum = temp1+temp2+carry;
 
-    if(sum > 9)
+    if(sum > 9)//进位
     {
     carry = 1;
     sum %= 10;
@@ -60,15 +61,14 @@ var addTwoNumbers = function(l1, l2) {
     {
     carry = 0;
     }
-
-    cur.next = new ListNode(sum);
+    cur.next = new ListNode(sum);//cur的后继节点设定为一个新的链表
     cur = cur.next;
     }
-
+    //当两个链表都为空后若还有进位，生成一个新的头节点为1的链表
     if(carry !== 0){
         cur.next = new ListNode(1);
     }
-    return ln.next;
+    return ln.next;//输出除了头节点之外的后继节点
   };
 //console.log(addTwoNumbers([2,4,3],[5,6,4]));
 console.log(addTwoNumbers([5,6,8],[5,6,4]));
