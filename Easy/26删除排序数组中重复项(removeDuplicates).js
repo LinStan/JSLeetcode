d9828485de5aae74d0d924c1363e172d23563347
LinStan <596636876@qq.com>
2018-09-20 09:42:51 +0800

    fix

--- Easy/26删除排序数组中重复项(removeDuplicates).js
+++ Easy/26删除排序数组中重复项(removeDuplicates).js
@@ -7,25 +7,28 @@
  * @param {number[]} nums
  * @return {number}
  */
- var nums = [1,2,2,5,6,6,6,6,6,9];
+ var nums = [1,2,2,5,6,6,6,6,6];
+//仅提交该函数部分
 var removeDuplicates = function(nums) {
   var len = nums.length;
-  var temp = 0;
+  var temp = 0;//计数
   if(len===0){return 0}
   for(var i=0;i<len;i++)
+  //遍历所有元素，若前后不同则保留前一个元素，相同则不予处理
   {
-    if(nums[i]!=nums[i+1])
+
+    if(nums[i]!=nums[i+1])//将不同元素移到数组前n个元素
     {
       nums[temp]=nums[i];
       temp = temp+1;
     }
+
   }
     return temp
 };
-//输出数组元素
+//输出数组元素，方便自己查看效果，不用提交
 var len = removeDuplicates(nums);
 for(var i=0;i<len;i++)
 {
   console.log(nums[i]);
 }
-//console.log(removeDuplicates([1,2,3]));
