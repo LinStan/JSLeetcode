7ed460bd5fa8c92842c209c2dcad974014be2c98
LinStan <596636876@qq.com>
2018-10-23 19:43:31 +0800

    learn

--- Learn/正则.js
+++ Learn/正则.js
@@ -7,5 +7,14 @@ function spacerpl(s)
   //替换第一次匹配的空格  \i代表单次匹配
   var s2 = s.replace(/\s/i,"first");
   console.log(s2);
+
+
 }
 console.log(spacerpl("wga aw wa ds s"));
+function normalrpl(s)
+{
+  //替换所有数字、大小写字母
+  var s1 = s.replace(/[0-9a-zA-Z]/g,"*")
+  console.log(s1);
+}
+console.log(normalrpl("saf 789 s89d"));
