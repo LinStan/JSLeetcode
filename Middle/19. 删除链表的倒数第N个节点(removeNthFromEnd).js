5ed95088ec1788af274c82b001ffcdee8b9e127e
LinStan <596636876@qq.com>
2018-10-24 10:00:53 +0800

    removeNthFromEnd

--- Middle/19. 删除链表的倒数第N个节点(removeNthFromEnd).js
+++ Middle/19. 删除链表的倒数第N个节点(removeNthFromEnd).js
@@ -1,5 +1,4 @@
 /**
- * 未完成！
  * @param {ListNode} head
  * @param {number} n
  * @return {ListNode}
@@ -8,35 +7,52 @@
        this.val = val;
        this.next = null;
  }
-function getl(head)
-{
-    var len = 0;
-    var cur = head;
-    while(cur!=null)
-        {
-            cur = cur.next;
-            len++;
-        }
-    return len;
-}
-var removeNthFromEnd = function(head, n) {
-    var l = getl(head);
-    console.log(l);
-    //if(n>l){return false}
-    var cur = new ListNode();
-    var cur1 = new ListNode();
+ /**
+  * Definition for singly-linked list.
+  * function ListNode(val) {
+  *     this.val = val;
+  *     this.next = null;
+  * }
+  */
+ /**
+  * @param {ListNode} head
+  * @param {number} n
+  * @return {ListNode}
+  */
+ //first method twice loop
+ var removeNthFromEnd = function(head, n) {
+     //build one Listnode which headnode = 0;
+     //dummy ListNode { val: 0, next: null }
+     var dummy = new ListNode(0);
+     //console.log(dummy);
+     dummy.next = head;
+     var l = 0;
+     //first ListNode { val: undefined, next: null }
+     var first = new ListNode();
+     //console.log(first);
+     //first 1->2->3->4->5
+     first = head;
+     //console.log(first)
+     //get head length
+     while(first!=null)
+     {
+         l++;
+         first = first.next;
+     }
+     l =l-n;
+     //first = dummy 0->1->2->3->4->5
+     first = dummy;
+     //console.log(first,dummy)
+     while(l>0)
+         {
+             l--;
+             first = first.next;
 
-    cur.next = head;
-    cur1.next = head;
-    console.log(l,cur,cur1,head);
-    while(n>0&&cur)
-    {
-      cur = cur.next;
-      n--;
-      console.log(cur,n);
-    }
-    if(n>0){return head}
+         }
 
-    return cur.next;
-};
+     first.next = first.next.next;
+     //dummy next 链接到first
+     console.log(first,dummy)
+     return dummy.next;
+ };
 console.log(removeNthFromEnd([1,2,3,4,5],2));
