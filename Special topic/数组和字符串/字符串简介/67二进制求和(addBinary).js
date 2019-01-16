7d33360f9da84d45af3841d399e553c7f5201445
LinStan <596636876@qq.com>
2019-01-16 17:05:52 +0800

    addBinary

--- Special topic/数组和字符串/字符串简介/67二进制求和(addBinary).js
+++ Special topic/数组和字符串/字符串简介/67二进制求和(addBinary).js
@@ -1,71 +1,52 @@
 /**
-给定两个二进制字符串，返回他们的和（用二进制表示）。
-输入为非空字符串且只包含数字 1 和 0。
  * @param {string} a
  * @param {string} b
  * @return {string}
  */
+ //已经战胜 11.84 % 的 javascript 提交记录
 var addBinary = function(a, b) {
-  var la = a.length;
-  var lb = b.length;
-  var flag = 0;
-  var sum =[];
-  a = a.split("");
-  b = b.split("");
-  //补全到相同长度
-  if(la<lb)
-  {
-    for(var i=0;i<lb-la;i++)
-    {
-      a.unshift('0');
-    }
-  }
-  if(la>lb)
-  {
-    for(var i=0;i<la-lb;i++)
-    {
-      b.unshift('0');
-    }
-  }
-  la = a.length;
-  lb = b.length;
-  console.log(a,b);
-  var l = Math.max(la,lb);
-  for(var i=0;i<l;i++)//四个条件判断
-  {
-    if((a[la-i-1]=="1"&&(b[lb-i-1]=="1")&&flag==0)
-    ||(a[la-i-1]=="1"&&(b[lb-i-1]=="0")&&flag==1)
-    ||(a[la-i-1]=="0"&&(b[lb-i-1]=="1")&&flag==1))
-    {
-      console.log(1);
-      flag=1;sum[i]=0;
-    }
-    else if((a[la-i-1]=="0"&&b[lb-i-1]=="0"&&flag==0))
-    {
-      console.log(a[la-i-1],b[lb-i-1]);
-      flag =0;sum[i]=0;
-    }
-    else if (a[la-i-1]=="1"&&b[lb-i-1]=="1"&&flag==1)
-    {
-      flag=1;sum[i]=1;
-    }
+   // a = a.split('');
+  //  b = b.split('');
+ //   console.log(a,b)
+    var re =[];
+    var flag = 0;
+    var al =a.length-1,bl = b.length-1;
+ //   var ll = Math.max(a.length,b.length);
+ //   console.log(ll)
+    for(let i = 0;i<Math.max(a.length,b.length);i++)
+        {
+      //      console.log(a[al-i],b[bl-i],flag,a[al-i]*1+b[bl-i]*1+flag)
+            let temp=0;
+            if(al-i<0)
+                {
+                    temp = b[bl-i]*1+flag;
+                }
+            else if(bl-i<0)
+                {
+                    temp = a[al-i]*1+flag;
+                }
+            else
+                {
+                    temp = a[al-i]*1+b[bl-i]*1+flag;
+                }
+            if(temp>=2)
+                {
+                    re[i] = temp-2;
+                    flag=1;
+                }
+                else
+                {
+                    re[i] = temp;
+                    flag=0;
+                }
+    //        console.log(re)
+        }
+    if(flag==1)
+        {
+            return 1+re.reverse().join("")
+        }
     else
-    {
-      console.log(a[la-i-1],b[lb-i-1],la-i-1,lb-i-1);
-      //console.log(3);
-      flag=0;sum[i]=1;
-    }
-  }
-  //console.log(i);
-  sum.reverse();
-  var temp =sum.slice(0);
-  if(i>=(l-1)&&flag==1)
-  {
-    temp.unshift(1);
-  }
-  temp = temp.join("");
-  return temp;
+        {
+                return re.reverse().join("")
+        }
 };
-console.log(addBinary("110","0"));
-console.log(addBinary("1","0"));
-console.log(addBinary("100","110010"));
