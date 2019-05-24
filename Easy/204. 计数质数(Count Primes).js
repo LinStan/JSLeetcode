92750abbe01e7755188cd60872b54195c0796a5d
LinStan <596636876@qq.com>
2019-05-24 16:54:56 +0800

    countPrimes

--- Easy/204. 计数质数(Count Primes).js
+++ Easy/204. 计数质数(Count Primes).js
@@ -1,25 +1,48 @@
+// 执行用时: 436 ms, 在Count Primes的JavaScript提交中击败了48 .88 % 的用户
+// 内存消耗: 134.7 MB, 在Count Primes的JavaScript提交中击败了11 .65 % 的用户
 /**
  * @param {number} n
  * @return {number}
  */
 var countPrimes = function (n) {
-    let ret = 0,
-        flag = 1;
-    if (n < 2) {
+    if (n <= 2) {
         return 0
     }
-    for (let i = 1; i < n; i++) {
-        for (let j = 2; j <= Math.sqrt(i); j++) {
-            if (i % j == 0) {
-                flag = 0;
-                break;
-            }
-        }
-        if (flag == 2) {
-            ret++;
-        } else {
-            flag = 2;
+    // 生成一个0-n-1的数组
+    let temp = [...new Array(n).keys()];
+    temp[0] = temp[1] = null
+    // console.log(temp)
+    for (let i = 2; i < n; i++) {
+        for (let j = 2; i * j <= n; j++) {
+            temp[i * j] = null;
         }
     }
-    return ret
+    //筛一下非null的值 filter是es6的一个数组方法，可以对数组进行一个筛选
+    temp = temp.filter(function (a) {
+        return a != null
+    })
+    // console.log(temp)
+    return temp.length
 };
+// 基础解法
+// var countPrimes = function (n) {
+//     let ret = 0,
+//         flag = 1;
+//     if (n < 2) {
+//         return 0
+//     }
+//     for (let i = 1; i < n; i++) {
+//         for (let j = 2; j <= Math.sqrt(i); j++) {
+//             if (i % j == 0) {
+//                 flag = 0;
+//                 break;
+//             }
+//         }
+//         if (flag == 2) {
+//             ret++;
+//         } else {
+//             flag = 2;
+//         }
+//     }
+//     return ret
+// };
