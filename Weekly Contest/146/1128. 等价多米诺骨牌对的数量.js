955d35fa6cf1c63bf7d542f93324940f7eeda7bd
Lin MSIK <596636876@qq.com>
2019-07-23 15:59:31 +0800

    fixed

--- Weekly Contest/146/1128. 等价多米诺骨牌对的数量.js
+++ Weekly Contest/146/1128. 等价多米诺骨牌对的数量.js
@@ -1,8 +1,27 @@
-let a = new Map();
-let b = [1, 2], c = [2, 1]
-if (!a.has(b.sort((a, b) => { return a - b }))) {
-  a.set(b, 1)
-}
-console.log(a)
-console.log(a.has([2, 1]))
-console.log(a.has(b))
+/**
+ * @param {number[][]} dominoes
+ * @return {number}
+ 基于Map实现
+ 注意一下Map只有传入字符串和数字才会被认定是同一个key，如果传入的是其他类型数据，即使一模一样也会被认定是不同的key
+ */
+var numEquivDominoPairs = function (dominoes) {
+  let tempArr = new Map(), l = dominoes.length, ret = 0;
+  for (let i = 0; i < l; i++) {
+    let ori = dominoes[i].sort((a, b) => { return a - b }).join('');
+    // console.log(ori,tempArr)
+    // console.log(tempArr.has(ori),ori)
+    if (!tempArr.has(ori)) {
+      tempArr.set(ori, 1)
+    }
+    else {
+      tempArr
+      tempArr.set(ori, tempArr.get(ori) + 1)
+    }
+  }
+  for (let [, value] of tempArr) {
+    if (value >= 2) {
+      ret += (value * (value - 1)) / 2
+    }
+  }
+  return ret
+};
